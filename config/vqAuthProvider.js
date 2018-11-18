61e379610339f823f64a135299a7ee8c7c4c2271
Adrian Barwicki <adrianbarwicki@gmail.com>
2018-11-18 09:52:58 +0100

    vqAuthProvider old

--- config/vqAuthProvider.js
+++ config/vqAuthProvider.js
@@ -1,6 +1,8 @@
+/**
 const ViciAuth = require('ViciAuthSDK')({
 	appKey: process.env.VICIGO_VA_APP_KEY,
 	apiKey: process.env.VICIGO_VA_API_KEY
 }, undefined, undefined , { host: 'localhost', port: 5000 } );
+*/
 
-module.exports = ViciAuth;
+module.exports = {};
